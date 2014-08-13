ad89dd256636eb8dc82244be633029d0410378b0
acab1633 <pqfpf+5yj6njvp9353k@sharklasers.com>
2014-08-13 20:09:36 +0000

    Create config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+/*config.h - config header for SipLib*/
+#ifndef _H_SIPLIB_CONFIG
+#define _H_SIPLIB_CONFIG
+/*general*/
+  #define SIPLIB_VERY_VERBOSE
+
+/*messages*/
+ #define SIPLIB_MAX_MESSAGE_SIZE        3000
+
+/*links*/
+
+#endif
